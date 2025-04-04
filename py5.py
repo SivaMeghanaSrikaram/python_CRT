f851690f4e8a90d240b8ebeec4cdca73dd8185a3
Meghana <228A1A05A1@risekrishnasaiprakasam.edu.in>
2025-04-04 10:28:18 +0530

    Add files via upload

---
+++ py5.py
@@ -0,0 +1,15 @@
+a=2025
+b='s1batch'
+c="rise"
+print('{2} {1} {0} students are learning python'.format(a,b,c))
+
+#here 2,1,0 are index numbers, format() supports only positive indexing
+
+print("-------------------------------------")
+
+
+a=2025
+b='s1batch'
+c="rise"
+d=f'{c} {b} {a} students are learning python.'
+print(d)
