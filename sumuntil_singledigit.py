508116368c07d4e7fbf4f13d7e266ffaab11ef74
Meghana <228A1A05A1@risekrishnasaiprakasam.edu.in>
2025-09-14 10:08:29 +0530

    Update sum_until_singledigit.py

--- sumuntil_singledigit.py
+++ sumuntil_singledigit.py
@@ -8,3 +8,11 @@ while(s==0 or s>=10):
         s=s+r
     n=s
 print(s)
+
+
+'''def addDigits(self, num):
+        if(num==0):
+            return 0
+        else:
+            num=(num-1)%9+1
+            return num'''
